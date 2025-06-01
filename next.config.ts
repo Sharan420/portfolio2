bfe26b1914e46f08b0e91366d8171e637717bf98
Sharan Suri <sharansuri-8@hotmail.com>
2025-06-01 13:36:02 +0530

    custom domain added

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  assetPrefix: "/portfolio2",
-  basePath: "/portfolio2",
 };
 
 export default nextConfig;
